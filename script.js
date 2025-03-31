c83c59400e4a4c010ead2aef783700740b74a266
Morteux24 <morteux3169@gmail.com>
2025-03-31 13:22:02 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,8 @@ const themeToggle = document.getElementById("theme-toggle");
 themeToggle.addEventListener("click", () => {
     document.body.classList.toggle("dark-theme");
     if (document.body.classList.contains("dark-theme")) {
-        document.body.style.backgroundColor = "black";
-        document.body.style.color = "white";
         themeToggle.textContent = "☀️";
     } else {
-        document.body.style.backgroundColor = "white";
-        document.body.style.color = "black";
         themeToggle.textContent = "🌙";
     }
 });
